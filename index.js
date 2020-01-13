3831733e587b2dd0819a7d3af81ec27dcaab4249
Eric De Felix <eric.defelix@leapdev.io>
2020-01-14 00:44:08 +1100

    checking in for revamp

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-// import './src/hc-leap.scss';
+import './src/hc-leap.scss';
 import './src/hc-ui';
+
+import category from './category.html';
+import discussions from "./discussions.html";
+import index from "./index.html";
